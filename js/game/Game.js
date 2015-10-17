9cc4b35c5b4b658294fb97f3a94ea4855d302b8d
David Goemans <dgoemans@gmail.com>
2015-10-17 17:34:36 +0000

    Initial repo setup

---
+++ js/game/Game.js
@@ -0,0 +1,21 @@
+define(["pixi"], 
+function(PIXI)
+{
+    function Game()
+    {
+        
+    };
+    
+    Game.prototype.update = function(delta)
+    {
+        
+    };
+    
+    Game.prototype.render = function()
+    {
+        
+    };
+    
+    
+    return Game;
+});
---
+++ js/main.js
@@ -0,0 +1,43 @@
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        pixi: '../bower_components/pixi/bin/pixi'
+    }
+});
+
+navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate || function(){};
+
+require(["game/Game", 
+    "pixi"], 
+function(Game, PIXI) {
+    var stage = new PIXI.Stage(0x6699FF);
+
+    var pixiRenderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+    document.body.appendChild(pixiRenderer.view);
+
+    var game = new Game(stage);
+
+    var lastTime = Date.now();
+
+    requestAnimationFrame( animate );
+
+    var delta = 0;
+
+    function animate() {
+
+        var now = Date.now();
+
+        delta = (now - lastTime)/1000;
+        delta = Math.min(delta,0.33);
+
+        requestAnimationFrame( animate );
+
+        game.update(delta);
+        game.render();
+
+        // render the stage
+        pixiRenderer.render(stage);
+
+        lastTime = now;
+    }
+});
